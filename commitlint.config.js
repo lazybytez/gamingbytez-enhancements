a4d8ff61327f81b70315a3dbf3db6d203925f426
Pascal Zarrad <p.zarrad@outlook.de>
2026-03-20 22:29:23 +0100

    ci: adjust commitlint config to match other projects
    Signed-off-by: Pascal Zarrad <p.zarrad@outlook.de>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,16 +1,24 @@
-module.exports = {
+export default {
+    extends: ['@commitlint/config-conventional'],
     rules: {
         'type-enum': [
             2,
             'always',
-            ['feat', 'fix', 'build', 'chore', 'ci', 'docs', 'perf', 'refactor', 'revert', 'style', 'test'],
+            [
+                'feat',
+                'fix',
+                'build',
+                'chore',
+                'ci',
+                'docs',
+                'perf',
+                'refactor',
+                'revert',
+                'style',
+                'test',
+                'sec',
+            ],
         ],
-        'type-case': [2, 'always', 'lower-case'],
-        'type-empty': [2, 'never'],
-        'scope-case': [2, 'always', 'lower-case'],
-        'subject-empty': [2, 'never'],
         'subject-max-length': [2, 'always', 50],
-        'subject-case': [2, 'always', 'lower-case'],
-        'header-max-length': [2, 'always', 72],
     },
 };
